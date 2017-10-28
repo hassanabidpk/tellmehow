c37c7795eecde87744d022da44c5d85210d62302
Hassan Abid <hassanabidpk89@gmail.com>
2017-10-28 09:24:22 +0900

    add template for home page

--- recycleapp/views.py
+++ recycleapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+
+
+def index(request):
+    context = {}
+    return render(request, "recycleapp/index.html", context)
+
