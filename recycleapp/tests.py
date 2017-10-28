667434e48b63b3adaacf293256efa6da2102a013
Hassan Abid <hassanabidpk89@gmail.com>
2017-10-28 18:02:38 +0900

    add basic json response test for server

--- recycleapp/tests.py
+++ recycleapp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Country, Category, Material, MainComponent, MinorComponent, Product, Favorite
+from .models import MainRecyclingInfomation, MinorRecyclingInfomation
 
-# Create your tests here.
+
+class CategoryTests(APITestCase):
+    def test_category_list(self):
+        """
+        Ensure we can get category list
+        """
+        # url = reverse('api/v1/category/list')
+        data = {}
+        response = self.client.get('http://128.199.69.81:8000/api/v1/category/list.json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class ComponentTests(APITestCase):
+    def test_component_list(self):
+        """
+        Ensure we can get category list
+        """
+        response = self.client.get('http://128.199.69.81:8000/api/v1/component/list.json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
